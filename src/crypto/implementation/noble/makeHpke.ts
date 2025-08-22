7804a1e84093a1586213712044e4c0e9c7c8d0ea
viniersiren <devinstuddard@gmail.com>
2025-08-22 11:50:46 -0700

    Add noble ciphersuite implementation

---
+++ src/crypto/implementation/noble/makeHpke.ts
@@ -0,0 +1,16 @@
+import { CipherSuite } from "@hpke/core"
+import { Hpke, HpkeAlgorithm, makeGenericHpke } from "../../hpke"
+import { makeAead } from "./makeAead"
+import { makeKdf } from "./makeKdfImpl"
+import { makeDhKem } from "./makeDhKem"
+
+export async function makeHpke(hpkealg: HpkeAlgorithm): Promise<Hpke> {
+  const [aead, aeadInterface] = await makeAead(hpkealg.aead)
+  const cs = new CipherSuite({
+    kem: await makeDhKem(hpkealg.kem),
+    kdf: makeKdf(hpkealg.kdf),
+    aead: aeadInterface,
+  })
+
+  return makeGenericHpke(hpkealg, aead, cs)
+}
