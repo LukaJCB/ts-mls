3d14d27dd4721ccb27bb3ee3fcfbae3d9316af05
ljacobowitz <luka.jacobowitz@gmail.com>
2026-04-21 17:29:47 -0400

    Micro performance improvements

--- src/codec/string.ts
+++ src/codec/string.ts
@@ -2,8 +2,9 @@ import { Decoder, mapDecoder } from "./tlsDecoder.js"
 import { Encoder, contramapBufferEncoder } from "./tlsEncoder.js"
 import { varLenDataDecoder, varLenDataEncoder } from "./variableLength.js"
 
-export const stringEncoder: Encoder<string> = contramapBufferEncoder(varLenDataEncoder, (s) =>
-  new TextEncoder().encode(s),
-)
+const _textEncoder = new TextEncoder()
+const _textDecoder = new TextDecoder()
 
-export const stringDecoder: Decoder<string> = mapDecoder(varLenDataDecoder, (u) => new TextDecoder().decode(u))
+export const stringEncoder: Encoder<string> = contramapBufferEncoder(varLenDataEncoder, (s) => _textEncoder.encode(s))
+
+export const stringDecoder: Decoder<string> = mapDecoder(varLenDataDecoder, (u) => _textDecoder.decode(u))
